76e1619bea4f74ec52120df42d983968e9852622
Lauren Hutchinson <Lauren Hutchinson>
2017-08-08 14:01:11 -0500

    copy solution

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "Mexico88$",
   database: "burgers_db"
 });
 
